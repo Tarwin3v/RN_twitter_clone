92778efc0de226951d70a681b22a2237901db32b
Tarwin3v <tarwin.ev@gmail.com>
2021-11-16 15:32:25 +0100

    persist user to db

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { StatusBar } from "expo-status-bar";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
@@ -7,8 +7,11 @@ import useColorScheme from "./hooks/useColorScheme";
 import Navigation from "./navigation";
 
 import { withAuthenticator } from "aws-amplify-react-native";
-import Amplify, { Analytics } from "aws-amplify";
+import Amplify, { Analytics, Auth, API, graphqlOperation } from "aws-amplify";
 import config from "./src/aws-exports.js";
+import { getUser } from "./graphql/queries";
+import { createUser } from "./graphql/mutations";
+import { CreateUserInput } from "./API";
 
 Amplify.configure({
   ...config,
@@ -20,6 +23,39 @@ Amplify.configure({
 function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
+
+  const getRandomImage = () => {
+    return "https://cdn.pixabay.com/photo/2020/12/12/17/24/little-egret-5826070_960_720.jpg";
+  };
+
+  const persistUser = async (user: CreateUserInput) => {
+    console.log(`User ${user}`);
+    await API.graphql(graphqlOperation(createUser, { input: user }));
+  };
+
+  useEffect(() => {
+    const updateUser = async () => {
+      const userInfo = await Auth.currentAuthenticatedUser({
+        bypassCache: true,
+      });
+      if (userInfo) {
+        const userData = await API.graphql(
+          graphqlOperation(getUser, { id: userInfo.attributes.sub })
+        );
+        if (!userData.data.getUser) {
+          const user = {
+            id: userInfo.attributes.sub,
+            username: userInfo.username,
+            name: userInfo.username,
+            email: userInfo.attributes.email,
+            image: getRandomImage(),
+          };
+          await persistUser(user);
+        }
+      }
+    };
+    updateUser();
+  }, []);
 
   if (!isLoadingComplete) {
     return null;
