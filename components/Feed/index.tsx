74c3057c3d9ba3aa8a9e7fd4dff95e3efa7dfbd6
Tarwin3v <tarwin.ev@gmail.com>
2021-11-17 15:37:51 +0100

    feed data

--- components/Feed/index.tsx
+++ components/Feed/index.tsx
@@ -1,17 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { FlatList } from "react-native";
-import { tweets } from "../../data/tweets";
 import { View } from "react-native";
 import Tweet from "../Tweet";
+import { API, graphqlOperation, Auth } from "aws-amplify";
+import { listTweets } from "../../graphql/queries";
 
-export type FeedProps = {};
+export const Feed = () => {
+  const [tweets, setTweets] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-export const Feed = (props: FeedProps) => (
-  <View style={{ width: "100%" }}>
-    <FlatList
-      data={tweets}
-      renderItem={({ item }) => <Tweet tweet={item} />}
-      keyExtractor={(item) => item.id}
-    />
-  </View>
-);
+  const getTweets = async () => {
+    setLoading(true);
+    try {
+      const tweetsData = await API.graphql(graphqlOperation(listTweets));
+      setTweets(tweetsData.data.listTweets.items);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getTweets();
+  }, []);
+
+  const propComparator = (propName) => (a, b) =>
+    a[propName] == b[propName] ? 0 : a[propName] > b[propName] ? -1 : 1;
+
+  return (
+    <View style={{ width: "100%", height: "100%" }}>
+      <FlatList
+        data={tweets.sort(propComparator("createdAt"))}
+        renderItem={({ item }) => <Tweet tweet={item} />}
+        keyExtractor={(item) => item.id}
+        refreshing={loading}
+        onRefresh={getTweets}
+      />
+    </View>
+  );
+};
